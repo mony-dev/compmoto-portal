9743dcbb9e110a8b4891155be406dacc822b0360
Khamonluck Angkananon <khamonluck.a@g-able.com>
2024-04-08 01:02:25 +0700

    fix error file

--- lib/shared/translations/i18n-config.ts
+++ lib/shared/translations/i18n-config.ts
@@ -1,27 +1,27 @@
-import { InitOptions } from "i18next";
-import { i18n as defaultConfig } from "../../../next-i18next.config";
+// // import { InitOptions } from "i18next";
+// import { i18n as defaultConfig } from "../../../next-i18next.config";
 
-export enum Language {
-  ENGLISH = "en-US",
-  THAI = "th-TH",
-  BURMESE = "my-MM",
-}
+// export enum Language {
+//   ENGLISH = "en-US",
+//   THAI = "th-TH",
+//   BURMESE = "my-MM",
+// }
 
-export enum LanguagePath {
-  ENGLISH = "en",
-  THAI = "th",
-  BURMESE = "my",
-}
+// export enum LanguagePath {
+//   ENGLISH = "en",
+//   THAI = "th",
+//   BURMESE = "my",
+// }
 
-export const fallbackLng = defaultConfig.defaultLocale;
+// export const fallbackLng = defaultConfig.defaultLocale;
 
-export const i18nextOptions: InitOptions = {
-  initImmediate: false,
-  fallbackLng: fallbackLng,
-  preload: defaultConfig.locales,
-  debug: false,
-  backend: {
-    loadPath: "public/locales/{{lng}}/{{ns}}.json",
-  },
-  ignoreJSONStructure: false,
-};
+// export const i18nextOptions: InitOptions = {
+//   initImmediate: false,
+//   fallbackLng: fallbackLng,
+//   preload: defaultConfig.locales,
+//   debug: false,
+//   backend: {
+//     loadPath: "public/locales/{{lng}}/{{ns}}.json",
+//   },
+//   ignoreJSONStructure: false,
+// };
