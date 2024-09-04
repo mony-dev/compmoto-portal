4c1583a1f22487e2ce1fec82e120d9fa19737997
khamonluck <khamonluck.ang@gmail.com>
2024-09-04 22:15:11 +0700

    check again

--- app/api/fetchInvoicesCopy/route.ts
+++ app/api/fetchInvoicesCopy/route.ts
@@ -57,127 +57,130 @@ export async function GET(request: Request) {
     // Parse the XML response
     const result = await parseStringPromise(response.data);
     const invoices = result['Soap:Envelope']['Soap:Body'][0]['ReportSalesInvoiceDetail_Result'][0]['p_oSales'][0]['PT_SalesInfo'];
-
+    const existingInvoice = await prisma.invoice.findFirst({
+      where: { documentNo: 'COM-IS2303-0012' }
+    });
     // Step 2: Process Each Invoice
-    for (const invoice of invoices) {
-      const invoiceNo = invoice['InvoiceNo'][0];
-      const postingDate = new Date(invoice['PostingDate'][0]);
-      const totalAmountString = invoice['TotalAmount'][0].replace(/,/g, ''); // Remove commas
-      const totalAmount = parseFloat(totalAmountString); // Parse to float
-      const custNo = invoice['CustNo'][0];
-      let externalDocument = null;
-      invoice['ExternalDoc'] ? externalDocument = invoice['ExternalDoc'][0] : null
-      console.error("invoiceNo:", invoiceNo);
-      // Check if the invoice already exists
-      const existingInvoice = await prisma.invoice.findFirst({
-        where: { documentNo: invoiceNo }
-      });
-      console.error("existingInvoice:", existingInvoice);
+    // for (const invoice of invoices) {
+    //   const invoiceNo = invoice['InvoiceNo'][0];
+    //   const postingDate = new Date(invoice['PostingDate'][0]);
+    //   const totalAmountString = invoice['TotalAmount'][0].replace(/,/g, ''); // Remove commas
+    //   const totalAmount = parseFloat(totalAmountString); // Parse to float
+    //   const custNo = invoice['CustNo'][0];
+    //   let externalDocument = null;
+    //   invoice['ExternalDoc'] ? externalDocument = invoice['ExternalDoc'][0] : null
+    //   console.error("invoiceNo:", invoiceNo);
+    //   // Check if the invoice already exists
+    //   const existingInvoice = await prisma.invoice.findFirst({
+    //     where: { documentNo: invoiceNo }
+    //   });
+    //   console.error("existingInvoice:", existingInvoice);
 
-      // if (!existingInvoice) {
-      //   // Step 3: Fetch Additional Invoice Details
-      //   const detailResponse = await axios({
-      //     method: 'get',
-      //     url: 'http://49.0.64.73:9147/BC200/WS/Comp%20Test/Codeunit/WSIntegration',
-      //     headers: {
-      //       'SOAPACTION': 'MasterSalesInvoice',
-      //       'Content-Type': 'application/xml',
-      //       'Authorization': 'Basic QURNMDFAY21jLmNvbTpDb21wbW90bzkq'
-      //     },
-      //     data: `<?xml version="1.0" encoding="UTF-8"?> 
-      //     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
-      //     xmlns:wsc="urn:microsoft-dynamics-schemas/codeunit/WSIntegration"> 
-      //     <soapenv:Body> 
-      //         <wsc:MasterSalesInvoice> 
-      //             <wsc:p_gInvoiceNo>${invoiceNo}</wsc:p_gInvoiceNo>          
-      //             <wsc:p_oSales></wsc:p_oSales>
-      //         </wsc:MasterSalesInvoice>        
-      //     </soapenv:Body> 
-      //     </soapenv:Envelope>`
-      //   });
+    //   // if (!existingInvoice) {
+    //   //   // Step 3: Fetch Additional Invoice Details
+    //   //   const detailResponse = await axios({
+    //   //     method: 'get',
+    //   //     url: 'http://49.0.64.73:9147/BC200/WS/Comp%20Test/Codeunit/WSIntegration',
+    //   //     headers: {
+    //   //       'SOAPACTION': 'MasterSalesInvoice',
+    //   //       'Content-Type': 'application/xml',
+    //   //       'Authorization': 'Basic QURNMDFAY21jLmNvbTpDb21wbW90bzkq'
+    //   //     },
+    //   //     data: `<?xml version="1.0" encoding="UTF-8"?> 
+    //   //     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
+    //   //     xmlns:wsc="urn:microsoft-dynamics-schemas/codeunit/WSIntegration"> 
+    //   //     <soapenv:Body> 
+    //   //         <wsc:MasterSalesInvoice> 
+    //   //             <wsc:p_gInvoiceNo>${invoiceNo}</wsc:p_gInvoiceNo>          
+    //   //             <wsc:p_oSales></wsc:p_oSales>
+    //   //         </wsc:MasterSalesInvoice>        
+    //   //     </soapenv:Body> 
+    //   //     </soapenv:Envelope>`
+    //   //   });
 
-      //   const detailResult = await parseStringPromise(detailResponse.data);
-      //   const salesInfo = detailResult['Soap:Envelope']['Soap:Body'][0]['MasterSalesInvoice_Result'][0]['p_oSales'][0]['PT_SalesInfo'][0];
+    //   //   const detailResult = await parseStringPromise(detailResponse.data);
+    //   //   const salesInfo = detailResult['Soap:Envelope']['Soap:Body'][0]['MasterSalesInvoice_Result'][0]['p_oSales'][0]['PT_SalesInfo'][0];
 
-      //   // Find userId based on custNo
-      //   const user = await prisma.user.findFirst({
-      //     where: { custNo }
-      //   });
+    //   //   // Find userId based on custNo
+    //   //   const user = await prisma.user.findFirst({
+    //   //     where: { custNo }
+    //   //   });
 
-      //   if (!user) {
-      //     logger.info(`User with customer number ${custNo} not found.`);
-      //     console.error(`User with customer number ${custNo} not found.`);
-      //     continue;
-      //   }
+    //   //   if (!user) {
+    //   //     logger.info(`User with customer number ${custNo} not found.`);
+    //   //     console.error(`User with customer number ${custNo} not found.`);
+    //   //     continue;
+    //   //   }
 
-      //   const userId = user.id;
-      //   logger.info(`userId ${userId} found.`);
+    //   //   const userId = user.id;
+    //   //   logger.info(`userId ${userId} found.`);
 
-      //   // Step 4: Calculate Total Qty
-      //   const lineItems = salesInfo['LineInfo'];
-      //   let totalQty = 0;
-      //   let subTotal =0;
-      //   for (const item of lineItems) {
-      //     const qty = parseInt(item['Qty'][0], 10);
-      //     const lineAmount = parseInt(item['LineAmount'][0], 10);
-      //     totalQty += qty;
-      //     subTotal += lineAmount;
-      //   }
+    //   //   // Step 4: Calculate Total Qty
+    //   //   const lineItems = salesInfo['LineInfo'];
+    //   //   let totalQty = 0;
+    //   //   let subTotal =0;
+    //   //   for (const item of lineItems) {
+    //   //     const qty = parseInt(item['Qty'][0], 10);
+    //   //     const lineAmount = parseInt(item['LineAmount'][0], 10);
+    //   //     totalQty += qty;
+    //   //     subTotal += lineAmount;
+    //   //   }
 
-      //   // Step 5: Save Invoice Data
-      //   const newInvoice = await prisma.invoice.create({
-      //     data: {
-      //       userId,
-      //       documentNo: invoiceNo,
-      //       date: postingDate,
-      //       totalPrice: totalAmount,
-      //       subTotal: subTotal,
-      //       totalAmount: totalQty,  // Add the calculated total quantity here
-      //       groupDiscount: user.custPriceGroup === '5STARS' ? 5 : 7,
-      //       externalDocument: externalDocument
-      //     }
-      //   });
+    //   //   // Step 5: Save Invoice Data
+    //   //   const newInvoice = await prisma.invoice.create({
+    //   //     data: {
+    //   //       userId,
+    //   //       documentNo: invoiceNo,
+    //   //       date: postingDate,
+    //   //       totalPrice: totalAmount,
+    //   //       subTotal: subTotal,
+    //   //       totalAmount: totalQty,  // Add the calculated total quantity here
+    //   //       groupDiscount: user.custPriceGroup === '5STARS' ? 5 : 7,
+    //   //       externalDocument: externalDocument
+    //   //     }
+    //   //   });
 
-      //   // Step 6: Save Invoice Items
-      //   for (const item of lineItems) {
-      //     const itemNo = item['ItemNo'][0];
-      //     const qty = parseInt(item['Qty'][0], 10);
-      //     const lineAmount = parseFloat(item['LineAmount'][0]);
-      //     const lineDiscountPc = parseFloat(item['LineDiscountPc'][0]);
-      //     const discountPc = parseFloat(item['LineAmtAfterDiscount'][0]);
+    //   //   // Step 6: Save Invoice Items
+    //   //   for (const item of lineItems) {
+    //   //     const itemNo = item['ItemNo'][0];
+    //   //     const qty = parseInt(item['Qty'][0], 10);
+    //   //     const lineAmount = parseFloat(item['LineAmount'][0]);
+    //   //     const lineDiscountPc = parseFloat(item['LineDiscountPc'][0]);
+    //   //     const discountPc = parseFloat(item['LineAmtAfterDiscount'][0]);
 
-      //     // Find productId based on itemNo (product code)
-      //     const product = await prisma.product.findUnique({
-      //       where: { code: itemNo }
-      //     });
+    //   //     // Find productId based on itemNo (product code)
+    //   //     const product = await prisma.product.findUnique({
+    //   //       where: { code: itemNo }
+    //   //     });
 
-      //     if (!product) {
-      //       logger.info(`Product with item number ${itemNo} not found.`);
-      //       console.error(`Product with item number ${itemNo} not found.`);
-      //       continue;
-      //     }
-      //     logger.info(`Product with item number ${itemNo} found.`);
+    //   //     if (!product) {
+    //   //       logger.info(`Product with item number ${itemNo} not found.`);
+    //   //       console.error(`Product with item number ${itemNo} not found.`);
+    //   //       continue;
+    //   //     }
+    //   //     logger.info(`Product with item number ${itemNo} found.`);
 
-      //     // Save the invoice item
-      //     const itemCreate = await prisma.invoiceItem.create({
-      //       data: {
-      //         invoiceId: newInvoice.id,
-      //         productId: product.id,
-      //         amount: qty,
-      //         price: (lineAmount/qty),
-      //         discount: lineDiscountPc,
-      //         discountPrice: discountPc
-      //       }
-      //     });
-      //     logger.info(`item created ${itemCreate}.`);
-      //   }
-      // } else {
-      //   logger.info(`Invoice ${invoiceNo} already exists. Skipping.`);
-      //   console.log(`Invoice ${invoiceNo} already exists. Skipping.`);
-      // }
-    }
+    //   //     // Save the invoice item
+    //   //     const itemCreate = await prisma.invoiceItem.create({
+    //   //       data: {
+    //   //         invoiceId: newInvoice.id,
+    //   //         productId: product.id,
+    //   //         amount: qty,
+    //   //         price: (lineAmount/qty),
+    //   //         discount: lineDiscountPc,
+    //   //         discountPrice: discountPc
+    //   //       }
+    //   //     });
+    //   //     logger.info(`item created ${itemCreate}.`);
+    //   //   }
+    //   // } else {
+    //   //   logger.info(`Invoice ${invoiceNo} already exists. Skipping.`);
+    //   //   console.log(`Invoice ${invoiceNo} already exists. Skipping.`);
+    //   // }
+    // }
     return NextResponse.json("200");
   } catch (error) {
+    console.error("existingInvoice:", error);
     return NextResponse.json(error);
   }
 }
