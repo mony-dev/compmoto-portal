d5b7b6b9d48b8bb649c7d2400797f89a51af321f
khamonluck <khamonluck.ang@gmail.com>
2024-09-06 11:03:30 +0700

    fix css

--- app/[locale]/(admin)/admin/(root)/(home)/media/page.tsx
+++ app/[locale]/(admin)/admin/(root)/(home)/media/page.tsx
@@ -203,7 +203,7 @@ const Media = () => {
           <div className="flex gap-2 items-center default-font">
             <div className="cursor-pointer hover:bg-comp-red h-full flex items-center px-4">
               <Link
-                className="hover:text-white"
+                className="hover:text-white text-white"
                 href={`/${locale}/admin/product?name=${brandName}`}
               >
                 {t("Product")}
@@ -211,7 +211,7 @@ const Media = () => {
             </div>
             <div className="cursor-pointer hover:bg-comp-red h-full flex items-center px-4">
               <Link
-                className="hover:text-white"
+                className="hover:text-white text-white"
                 href={`/${locale}/admin/news?name=${brandName}`}
               >
                 {t("News and events")}
@@ -223,7 +223,7 @@ const Media = () => {
               }`}
             >
               <Link
-                className="hover:text-white"
+                className="hover:text-white text-white"
                 href={`/${locale}/admin/media?name=${brandName}`}
               >
                 {t("Marketing")}
