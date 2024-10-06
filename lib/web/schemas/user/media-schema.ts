b3e4f88952dcfe5ce26996af96c71e5dbcba7e53
khamonluck <khamonluck.ang@gmail.com>
2024-10-06 21:24:50 +0700

    revert

--- lib/web/schemas/user/media-schema.ts
+++ lib/web/schemas/user/media-schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 
 export const mediaSchema = z.object({
   minisizeId: z.number().nonnegative({ message: "Minisize is required!" }),
-  name: z.string().optional() ,
+  name: z.string().nonempty("Name is required!"),
   isActive: z.boolean().default(true),
   url: z.string().optional() ,
   duration: z.string().optional() ,
