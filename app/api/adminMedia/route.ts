82ded98be6c0f4457b02ab175d2a9297c779c868
khamonluck <khamonluck.ang@gmail.com>
2024-10-06 21:05:52 +0700

    fix media

--- app/api/adminMedia/route.ts
+++ app/api/adminMedia/route.ts
@@ -46,7 +46,7 @@ export async function GET(request: Request) {
       whereClause.AND.push({ isActive });
     }
 
-    if (minisizeId !== undefined) {
+    if (minisizeId) {
       whereClause.AND.push({ minisizeId: Number(minisizeId) });
     }
 
